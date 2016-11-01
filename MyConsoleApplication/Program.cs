389a31e67dceb14c0ebc2e983f7f20025ca87427
Student <Student@FJPC-13>
2016-11-01 16:26:59 +0200

    Array's excersise

--- MyConsoleApplication/Program.cs
+++ MyConsoleApplication/Program.cs
@@ -10,7 +10,38 @@ namespace MyConsoleApplication
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Please enter a number:");
+            String input = System.Console.ReadLine();
+            int size = 0;
+            Int32.TryParse(input, out size);
 
+            int[][] matrix = new int[size][];
+            int buf = 0;
+
+            for (int m = 0; m < size; m++)
+            {
+                matrix[m] = new int[size];
+                for (int n = 0; n < size; n++)
+                {
+                    matrix[m][n] = buf++;
+                    System.Console.WriteLine(matrix[m][n]);
+                }
+            }
+
+            System.Console.WriteLine("Please enter first index of the element (max is "+ (size-1) + "): ");
+            String input1 = System.Console.ReadLine();
+            int firstIndex = 0;
+            Int32.TryParse(input1, out firstIndex);
+
+            System.Console.WriteLine("Please enter second index of the element (max is " + (size-1) + "): ");
+            String input2 = System.Console.ReadLine();
+            int secondIndex = 0;
+            Int32.TryParse(input1, out secondIndex);
+
+            matrix[firstIndex][secondIndex]++;
+            System.Console.WriteLine(matrix[firstIndex][secondIndex]);
+
+            System.Console.ReadLine();
         }
     }
 }
